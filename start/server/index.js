4576cc7a481a51538e1896a857d82d1a27067509
alam-mashrufee <mashrufee@gmail.com>
2021-10-18 13:09:57 +0900

    server index file

---
+++ start/server/index.js
@@ -0,0 +1,10 @@
+const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
