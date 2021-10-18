104c6d5e40288f82aed964bad1e1a4d8e43c16bb
alam-mashrufee <mashrufee@gmail.com>
2021-10-18 16:15:23 +0900

    Apollo server

---
+++ start/server/DB.js
@@ -0,0 +1,31 @@
+const mainCards = [
+    {
+      title: 'Recently Viewed',
+      image: 'lion',
+    },
+    {
+      title: 'Looking for a gift',
+      image: 'penguin',
+    },
+    {
+        title: 'Best Behaved',
+        image: 'cat',
+    },
+  ];
+
+  const animals = [
+    {
+        title: "Cat",
+        image: "https://test.jp",
+        rating: 3.4,
+        price: "4",
+        description:["desc"],
+        stock: 4,
+        onSale: true
+    }
+  ];
+
+  module.exports = {
+      mainCards,
+      animals
+  }
--- start/server/index.js
+++ start/server/index.js
@@ -1,10 +1,43 @@
 const { ApolloServer, gql } = require('apollo-server');
+const {mainCards, animals} = require('./DB');
+
+// schema
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  type Animal {
+    title: String!
+    image: String!
+    rating: Float
+    price: String!
+    description: [String!]!
+    stock: Int!
+    onSale: Boolean
   }
+
+  type MainCard {
+        title: String!
+        image: String!
+    }
+
+# Query
   type Query {
-    books: [Book]
+    mainCards: [MainCard]
+    animals: [Animal]
   }
 `;
+
+  // resolver
+const resolvers = {
+    Query: {
+      mainCards: () => mainCards,
+      animals: () => animals,
+    },
+  };
+  
+  // The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
