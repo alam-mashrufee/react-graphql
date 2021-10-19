57d187987aee2a3b3ea3b89cb77fd2f61157a4a0
alam-mashrufee <mashrufee@gmail.com>
2021-10-19 20:53:10 +0900

    Adding Mutation

--- start/client/src/pages/LandingPage.js
+++ start/client/src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MainHero from "../components/MainHero/MainHero"
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
 import CardDisplay from "../components/CardDisplay/CardDisplay"
-import {useQuery, gql} from "@apollo/client";
+import {useQuery, useMutation, gql} from "@apollo/client"
 
 const ANIMALS_QUERY = gql`
 {
@@ -16,9 +16,35 @@ const ANIMALS_QUERY = gql`
     }
 }`;
 
+const ADD_ANIMAL_MUTATION = gql`
+mutation (
+    $image: String!,
+    $category: String!,
+    $title: String!,
+    $stock: String!,
+    $price: String!,
+    $description: [String!]!,
+    $rating: String!,
+    $slug: String!,
+) {
+  addAnimal(
+    image: $image,
+    category: $category,
+    title: $title,
+    stock: $stock,
+    price: $price,
+    description: $description,
+    rating: $rating,
+    slug: $slug,
+    ) {
+       id
+    }
+}`;
+
 function LandingPage() {
 
     const {loading, error, data} = useQuery(ANIMALS_QUERY);
+    const [addAnimal] = useMutation(ADD_ANIMAL_MUTATION);
 
     if(loading) return <div>loading...</div>
     if(error) return <div>some error happened</div>
@@ -28,6 +54,20 @@ function LandingPage() {
             <MainHero />
             <CategoryDisplay />
             <CardDisplay animals={data.animals}/>
+            <button onClick={() => {
+                addAnimal({
+                    variables: {
+                        image: "ostrich",
+                        category: "1",
+                        title: "this is really cool ostrich",
+                        stock: 13,
+                        price: "32,322",
+                        description: ["description of this ostrich"],
+                        rating: 3.5,
+                        slug: "ostrich",
+                    }
+                })
+            }}>Add an new Animal</button>
         </div>
     )
 }
